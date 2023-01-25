14a1d8bb1f0619993370d3476c3bd9592bf931d7
jihun <jihunv8@gmail.com>
2023-01-25 11:28:32 +0900

    [fix] 배너 교체시간 줄임

--- src/components/home/main-banner/MainBanner.tsx
+++ src/components/home/main-banner/MainBanner.tsx
@@ -9,12 +9,27 @@ import { bannerItems, bannerNavItems } from './bannerData';
 const MainBanner = (): JSX.Element => {
   const [bannerIndex, setBannerIndex] = useState(0);
 
+  // 배너 바뀌는 모습 보여주기 위한 코드. 기존 시간은 배너가 바뀌는 모습을 못보고 지나칠 가능성이 있어서 작성함
+  // 배너 렌더링 후 처음 배너교체 시간 1초.
   useEffect(() => {
     const autoBannerChangeTimer = setTimeout(() => {
       let nextBannerIndex = bannerIndex + 1;
 
       setBannerIndex(nextBannerIndex < 4 ? nextBannerIndex : 0);
-    }, 10000);
+    }, 1000);
+
+    return () => {
+      clearTimeout(autoBannerChangeTimer);
+    };
+  }, []);
+  // 배너 바뀌는 모습 보여주기 위한 코드 여기까지....
+
+  useEffect(() => {
+    const autoBannerChangeTimer = setTimeout(() => {
+      let nextBannerIndex = bannerIndex + 1;
+
+      setBannerIndex(nextBannerIndex < 4 ? nextBannerIndex : 0);
+    }, 5000);
 
     return () => {
       clearTimeout(autoBannerChangeTimer);
